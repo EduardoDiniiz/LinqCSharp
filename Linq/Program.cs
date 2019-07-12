8d54a5179d803e293e20b42ac49f44dda07a4d07
EduardoDiniiz <eduardocco11@gmail.com>
2019-07-12 00:17:17 -0300

    Exercicio utilizando LINQ

---
+++ Linq/Program.cs
@@ -0,0 +1,38 @@
+﻿using Linq.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    list.Add(new Product(line[0], double.Parse(line[1], CultureInfo.InvariantCulture)));
+                }
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
